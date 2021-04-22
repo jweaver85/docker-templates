33cac9cd3613c8ee3120f4e7506f104d06b3fbab
jweaver <justinweaver1985@gmail.com>
2021-04-22 16:37:49 -0600

    updates python-http-webserver with a better example and working Dockerfile

---
+++ python-http-webserver/example/Readme.md
@@ -0,0 +1,18 @@
+#Python HTTP Webserver
+
+This is a simple Python http webserver 
+
+Prerequisites
+---
+Python 3
+
+Setup
+---
+- ``./buildDocker.sh``
+- ``docker run -it -p 8000:8000 python-http-webserver``
+
+links
+---
+- http://localhost:8000/api/mulitiply/2/4/
+- http://localhost:8000/api/square/4/
+- http://localhost:8000/
---
+++ python-http-webserver/example/server.py
@@ -0,0 +1,37 @@
+import socketserver
+import http.server
+import re
+
+PORT = 8000
+
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if None is not re.search('/api/square/*', self.path):
+            num = float(self.path.split('/')[-1])
+            print(self.path.split('/'))
+            # This URL will trigger our sample function and send what it returns back to the browser
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(str(num*num).encode())  # call sample function here
+            return
+        if None is not re.search('/api/multiply/*', self.path):
+            num1 = float(self.path.split('/')[-1])
+            num2 = float(self.path.split('/')[-2])
+            # This URL will trigger our sample function and send what it returns back to the browser
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(str(num1*num2).encode())  # call sample function here
+            return
+        else:
+            # serve files, and directory listings by following self.path from
+            # current working directory
+            http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+
+httpd = socketserver.ThreadingTCPServer(('', PORT),CustomHandler)
+
+print("serving at port", PORT)
+httpd.serve_forever()
