94c17a2b22f5b5b1e71b388dc62ab98f7d589334
jweaver <justinweaver1985@gmail.com>
2021-04-23 19:46:59 -0600

    updates node-express-webserver, experiments with list URI parameters

--- node-express-webserver/example/Dockerfile
+++ node-express-webserver/example/Dockerfile
@@ -1,4 +1,4 @@
-FROM node:15
+FROM node:15-alpine
 
 # Create app directory
 WORKDIR /usr/src/app
--- node-express-webserver/example/server.js
+++ node-express-webserver/example/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const _ = require('lodash');
 
 // Constants
 const PORT = 8080;
@@ -10,6 +11,34 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/hello-world', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/sort-numbers', (req, res) => {
+    let listParam = req.query.list;
+    let list = [];
+    if(listParam) {
+        list = listParam.split(',').map(item => parseInt(item));
+        list = _.sortBy(list)
+    }
+    console.log(list);
+    res.send(list)
+});
+
+app.get('/sort-strings', (req, res) => {
+   let listParam = req.query.list;
+   let list = [];
+   if(listParam) {
+       list = listParam.split(',');
+       list = _.sortBy(list);
+   }
+   console.log(list);
+   res.send(list);
+});
+
+app.get('/sort-number-array', (req, res) => {
+   let list = JSON.parse(req.query.list);
+   console.log(list);
+   res.send(_.sortBy(list))
 });
 
 app.listen(PORT, HOST);
